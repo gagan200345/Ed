eec1d4e6e2553e9e5ba9b704eb69fb8b93124f18
Gagan khandelwal <gaagankhandelwal@gmail.com>
2026-09-06 21:37:42 +0530

    allow vercel frontend in cors

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ed-frontend-zeta.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
